21ab556f204be26b0ccb014c58b5c4dc5ee4df64
rohankaliacodes <rkalia4@uwo.ca>
2023-12-06 22:17:21 -0500

    preprocess_data function data validation and prep

--- product_recommendation_system.py
+++ product_recommendation_system.py
@@ -8,3 +8,15 @@ def load_data(file_path):
         return data
     except FileNotFoundError:
         return "File not found. Please check the file path."
+    
+def preprocess_data(grocery_data):
+    required_columns = ['Order ID', 'Customer Name', 'Category', 'Sub Category']
+    if not all(col in grocery_data.columns for col in required_columns):
+        return "Data missing required columns."
+    grocery_data.dropna(subset=required_columns, inplace=True)
+    unnecessary_columns = set(grocery_data.columns) - set(required_columns)
+    grocery_data.drop(columns=unnecessary_columns, inplace=True, errors='ignore')
+    customer_profiles = grocery_data.groupby(['Customer Name', 'Category', 'Sub Category']).size().reset_index(name='Purchase Count')
+    customer_profiles['Features'] = customer_profiles.apply(lambda x: (x['Category'] + ' ' + x['Sub Category'] + ' ') * x['Purchase Count'], axis=1)
+    aggregated_profiles = customer_profiles.groupby('Customer Name')['Features'].sum().reset_index()
+    return aggregated_profiles, customer_profiles
