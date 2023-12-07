e3f02f6db8e7fe2d94a466379228894f4c9775b0
rohankaliacodes <rkalia4@uwo.ca>
2023-12-06 22:18:07 -0500

    recommend_products function for recommendations

--- product_recommendation_system.py
+++ product_recommendation_system.py
@@ -20,3 +20,21 @@ def preprocess_data(grocery_data):
     customer_profiles['Features'] = customer_profiles.apply(lambda x: (x['Category'] + ' ' + x['Sub Category'] + ' ') * x['Purchase Count'], axis=1)
     aggregated_profiles = customer_profiles.groupby('Customer Name')['Features'].sum().reset_index()
     return aggregated_profiles, customer_profiles
+
+def recommend_products(aggregated_profiles, customer_profiles, customer_name, top_n=5):
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(aggregated_profiles['Features'])
+    if customer_name not in aggregated_profiles['Customer Name'].values:
+        return "Customer not found."
+    idx = aggregated_profiles.index[aggregated_profiles['Customer Name'] == customer_name].tolist()[0]
+    cosine_sim = cosine_similarity(count_matrix)
+    similarity_scores = list(enumerate(cosine_sim[idx]))
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    top_customers_indices = [i[0] for i in similarity_scores[1:top_n+1]]
+    recommended_categories = customer_profiles[customer_profiles['Customer Name'].isin(aggregated_profiles.iloc[top_customers_indices]['Customer Name'])].drop_duplicates()
+    recommendations = recommended_categories.groupby(['Category', 'Sub Category']).size().reset_index(name='Counts')
+    recommendations = recommendations.sort_values(by='Counts', ascending=False).head(top_n)
+    formatted_recommendations = ""
+    for index, row in recommendations.iterrows():
+        formatted_recommendations += f"- {row['Category']} > {row['Sub Category']} (recommended based on {row['Counts']} similar purchases)\n"
+    return f"Based on your purchase history and similar customers' preferences, we recommend the following products:\n{formatted_recommendations}"
